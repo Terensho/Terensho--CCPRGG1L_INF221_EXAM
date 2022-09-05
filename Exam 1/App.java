4497d2d7c9a58bdbea39ede878ddddd7edd944ed
Dionela, Terrence A <111472732+Terensho@users.noreply.github.com>
2022-09-05 09:50:06 +0800

    Create App.java

---
+++ Exam 1/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) {
+      Student ME = new Student();
+      ME . surName = "Dionela";
+      ME . firstName = "Terrence";
+      ME . middleInitial = 'A';
+      ME . dateOfBirth = 11/16/03;
+      ME . studentNumber = 2022101823;
+      ME . studentEmailAddress = "dionelata@students.national-u.edu.ph";
+      ME . iAmAwesome = true;
+      ME . sayMyStudentNumber();
+      ME . studentEmailAddress();
+      ME . iAmAwesome();
+    }
+}
